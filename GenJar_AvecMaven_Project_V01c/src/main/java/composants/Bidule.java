e7397ee61a5d5726dc14c3dfd07bdc19873f6228
herve.marchal <herve.marchal@hotmail.fr>
2016-04-22 17:52:09 +0200

    Ajout classe Bidule

---
+++ GenJar_AvecMaven_Project_V01c/src/main/java/composants/Bidule.java
@@ -0,0 +1,25 @@
+package composants;
+
+public class Bidule {
+	String truc;
+	String machin;
+	String chose;
+	public String getTruc() {
+		return truc;
+	}
+	public void setTruc(String truc) {
+		this.truc = truc;
+	}
+	public String getMachin() {
+		return machin;
+	}
+	public void setMachin(String machin) {
+		this.machin = machin;
+	}
+	public String getChose() {
+		return chose;
+	}
+	public void setChose(String chose) {
+		this.chose = chose;
+	}
+}
